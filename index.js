196f44c5e679de7473f6d34511c5d0b0931a2fe9
Paul Vacey <pvacey@gmail.com>
2026-01-11 15:01:42 -0600

    update

--- index.js
+++ index.js
@@ -1,105 +1,143 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
-
-const GUILD_ID = '790021340509896704';
-const CHANNEL_ID = '790021340509896708';
-let guild = null;
-let channel = null;
-let connection = null;
-const player = createAudioPlayer();
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildVoiceStates, // Essential for voice!
-	],
-});
+const { createAudioPlayer, createAudioResource, getVoiceConnections, joinVoiceChannel, VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
 
 
-const addBotToVoice = () => {
-  try {
-      connection = joinVoiceChannel({
-          channelId: channel.id,
-          guildId: guild.id,
-          adapterCreator: guild.voiceAdapterCreator,
-          selfDeaf: true, 
-          selfMute: false,
-      });
+class VoiceConnection {
+  constructor(guildId, channelId, client) {
+    this.player = createAudioPlayer();
+    this.guild = client.guilds.cache.get(guildId);
+    this.channel = this.guild.channels.cache.get(channelId);
 
-      connection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
-      	console.log('Connection is in the Ready state!');
-      });
-      
-      
-      console.log(`Successfully joined ${channel.name}`);
-  } catch (error) {
-      console.error("Error joining voice channel:", error);
+    this.connection = joinVoiceChannel({
+      channelId: channelId,
+      guildId: guildId,
+      adapterCreator: this.guild.voiceAdapterCreator,
+      selfDeaf: true,
+      selfMute: false,
+    });
+
+    this.connection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
+      console.log(`voice connection opened @${this.guild.name} -> ${this.channel.name}`);
+    });
+    
+    this.connection.on(VoiceConnectionStatus.Disconnected, (oldState, newState) => {
+      console.log(`voice connection closed @${this.guild.name} -> ${this.channel.name}`);
+    });
   }
-};
 
+  playSound(fileName) {
+    const subscription = this.connection.subscribe(this.player);
+    const resource = createAudioResource(fileName);
+    
+    this.player.on(AudioPlayerStatus.Playing, (oldState, newState) => {
+      console.log(`playing ${fileName} @${this.guild.name} -> ${this.channel.name}`);
+    });
 
-const playSound = (fileName) => {
-  const subscription = connection.subscribe(player);
-  const resource = createAudioResource(fileName);
+    this.player.play(resource);
 
-  player.play(resource);
-  player.on(AudioPlayerStatus.Playing, (oldState, newState) => {
-  	console.log('Audio player is in the Playing state!');
-  });
-
-  if (subscription) {
-  	setTimeout(() => { subscription.unsubscribe() }, 5_000);
+    if (subscription) {
+      setTimeout(() => {
+        subscription.unsubscribe()
+      }, 5_000);
+    }
   }
 }
 
+///////////////////////////////////////////////////////////
+// Discord Event Listeners                               //
+///////////////////////////////////////////////////////////
 
-client.once('clientReady', async () => {
-    console.log(`Logged in as ${client.user.tag}`);
+const connections = {}
 
-    guild = client.guilds.cache.get(GUILD_ID);
-    if (!guild) return console.error("Guild not found.");
-
-    channel = guild.channels.cache.get(CHANNEL_ID);
-    if (!channel) return console.error("Channel not found.");
-
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates, // Essential for voice!
+  ],
 });
 
+client.once(Events.ClientReady, async () => {
+  console.log(`bot logged in as ${client.user.tag}`);
+});
 
 client.on(Events.VoiceStateUpdate, (oldState, newState) => {
-    // 1. User Joined a channel (wasn't in one, now is)
-    if (!oldState.channelId && newState.channelId) {
-      console.log(`${newState.member.user.tag} joined ${newState.channel.name}`);
-      if (!connection) {
-        addBotToVoice()
-      }
-      else {
-        playSound("holyshit-quake.mp3");
-      }
-    }
+  // user joined a channel and it's not the bot itself
+  if (!oldState.channelId && newState.channelId && newState.member.user.id != client.user.id) {
+    console.log(`${newState.member.user.tag} joined ${newState.channel.name}`);
+    
+    // if there isn't a connection, make one
+    if (!connections[newState.channelId]) {
+      connections[newState.channelId] = new VoiceConnection(newState.guild.id, newState.channelId, client)
+    } 
+    // play sound
+    connections[newState.channelId].playSound("holyshit-quake.mp3")
+  }
 
-    // 2. User Left a channel (was in one, now isn't)
-    if (oldState.channelId && !newState.channelId) {
-      if (oldState.channel.members.size == 1 && connection != null) {
-        connection.destroy()
-        connection = null;
-      }
-      console.log(`${oldState.member.user.tag} left ${oldState.channel.name}`);
-    }
+  // user left a channel, cleanup
+  if (oldState.channelId && !newState.channelId) {
+    console.log(`${oldState.member.user.tag} left ${oldState.channel.name}`);
+    if (oldState.channel.members.size == 1 && connections[oldState.channelId]) {
+      connections[oldState.channelId].connection.destroy()
+      delete connections[oldState.channelId]
 
-    // 3. User Switched channels
-    if (oldState.channelId && newState.channelId && oldState.channelId !== newState.channelId) {
-      if (oldState.channel.members.size == 1 && connection != null) {
-        connection.destroy()
-        connection = null;
-      }
-      console.log(`${newState.member.user.tag} moved from ${oldState.channel.name} to ${newState.channel.name}`);
     }
+  }
 });
-
 
 client.login(process.env.DISCORD_TOKEN);
 
-// client.on('messageCreate', (message) => {
-//   console.log(message)
-// });
+///////////////////////////////////////////////////////////
+// DOTA2 GSI Server                                      //
+///////////////////////////////////////////////////////////
+
+const d2gsi = require('dota2-gsi');
+const server = new d2gsi();
+
+const mapping = {
+  "player:deaths": {
+    sound: "stinks.mp3",
+    condition: ">",
+    value: 0
+  },
+  "hero:level": {
+    sound: "stinks.mp3",
+    condition: ">",
+    value: 1
+  }
+}
+
+const dotaClients = []
+
+server.events.on('newclient', function(client) {
+  
+  for (const [eventName,v] of Object.entries(mapping)) {
+    client.on(eventName, (v) => {
+      let condition = false;
+      switch(v.condition) {
+        case "*":
+          condition = true;
+          break
+        case ">":
+          condition = v.condition + " > " + v.value;
+          break;
+        case "<":
+          condition = v.condition + " < " + v.value;
+          break;
+        case "===":
+          condition = v.condition + " === " + v.value;
+          break;
+        case "!==":
+          condition = v.condition + " !== " + v.value;
+          break;
+        default:
+          console.log(`failed to handle mapping ${eventName = {v}")
+      }
+      // this is just building a string, won't work but saving progress
+      if (condition) {
+        playSound(v.sound);
+      }
+    });
+  }
+});
