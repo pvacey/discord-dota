990fb09f753de94898d4b70ab5b541dd744b7389
Paul Vacey <pvacey@gmail.com>
2026-03-16 19:46:42 -0500

    improve logging, make the match summary function parse the match as well

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import { createAudioPlayer, createAudioResource, getVoiceConnections, joinVoiceC
 import { watch } from 'fs';
 import fs from 'node:fs';
 import path from 'node:path';
+import pino from 'pino';
+
+const logger = pino({
+  level: 'info',
+  timestamp: () => `,"time":"${new Date().toISOString()}"`,
+})
 
 class VoiceConnection {
   constructor(guildId, channelId, client) {
@@ -20,20 +26,16 @@ class VoiceConnection {
     });
 
     this.connection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
-      console.log(`voice connection opened @${this.guild.name} -> ${this.channel.name}`);
+      logger.info(`voice connection opened @${this.guild.name} -> ${this.channel.name}`);
     });
     
     this.connection.on(VoiceConnectionStatus.Disconnected, (oldState, newState) => {
-      console.log(`voice connection closed @${this.guild.name} -> ${this.channel.name}`);
-    });
-    
-    this.player.on(AudioPlayerStatus.Playing, (oldState, newState) => {
-      console.log(`playing sound @${this.guild.name} -> ${this.channel.name}`);
+      logger.info(`voice connection closed @${this.guild.name} -> ${this.channel.name}`);
     });
   }
 
   playSound(fileName) {
-    console.log('invoked once')
+    logger.info(`playing sound ${fileName} @${this.guild.name} -> ${this.channel.name}`);
     if (!this.subscription) {
       this.subscription = this.connection.subscribe(this.player);
     }
@@ -73,7 +75,7 @@ for (const folder of commandFolders) {
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			logger.warn(`The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
 }
@@ -83,14 +85,14 @@ client.on(Events.InteractionCreate, async (interaction) => {
 	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
+		logger.error(`No command matching ${interaction.commandName} was found.`);
 		return;
 	}
 
 	try {
 		await command.execute(interaction);
 	} catch (error) {
-		console.error(error);
+		logger.error(error);
 		if (interaction.replied || interaction.deferred) {
 			await interaction.followUp({
 				content: 'There was an error while executing this command!',
@@ -107,13 +109,13 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
 
 client.once(Events.ClientReady, async () => {
-  console.log(`bot logged in as ${client.user.tag}`);
+  logger.info(`bot logged in as ${client.user.tag}`);
 });
 
 client.on(Events.VoiceStateUpdate, (oldState, newState) => {
   // user joined a channel and it's not the bot itself
   if (!oldState.channelId && newState.channelId && newState.member.user.id != client.user.id) {
-    console.log(`${newState.member.user.tag} joined ${newState.channel.name}`);
+    logger.info(`${newState.member.user.tag} joined ${newState.channel.name}`);
     
     // if there isn't a connection, make one
     if (!connections[newState.channelId]) {
@@ -125,7 +127,7 @@ client.on(Events.VoiceStateUpdate, (oldState, newState) => {
 
   // user left a channel, cleanup4
   if (oldState.channelId && !newState.channelId) {
-    console.log(`${oldState.member.user.tag} left ${oldState.channel.name}`);
+    logger.info(`${oldState.member.user.tag} left ${oldState.channel.name}`);
     if (oldState.channel.members.size == 1 && connections[oldState.channelId]) {
       connections[oldState.channelId].connection.destroy()
       delete connections[oldState.channelId]
@@ -154,22 +156,32 @@ const recursiveDiff = (prefix, changed, body, context) => {
   }
 }
 
+const gameSummary = async (matchID) => {
+  suppressReport = true
+  setTimeout(() => {
+    suppressReport = false
+  }, 5000);
+
+  const f = Bun.file('settings.json');
+  if (f.exists()) {
+    let settings = await f.json()
+    const channel = await client.channels.fetch(settings.channel);
+    channel.send(`https://www.opendota.com/matches/${matchID}`);
+    console.log({channel})
+    logger.info(`sent match details to ${channel.guild.name} -> ${channel.name}`);
+    
+    const response = await fetch(`http://api.opendota.com/api/request/${matchID}`, {
+      method: "POST"
+    });
+    logger.info(`opendota parse request for matchID=${matchID} http_status=${response.status}`)
+  }
+}
+
 const handleGameEvent = async (eventName, value, context) => {
-  console.log(`checking mapping to handle ${eventName}=${value} playerID=${context.playerID}`)
+  logger.debug(`handling event ${eventName}=${value} playerID=${context.playerID}`)
 
   if (eventName === "map.game_state" && value === "DOTA_GAMERULES_STATE_POST_GAME" && !suppressReport) {
-    
-    suppressReport = true
-    setTimeout(() => {
-      suppressReport = false
-    }, 5000);
-
-    const f = Bun.file('settings.json');
-    if (f.exists()) {
-      let settings = await f.json()
-      const channel = await client.channels.fetch(settings.channel);
-      channel.send(`https://www.opendota.com/matches/${context.matchID}`);
-    }
+    gameSummary(context.matchID);
   }
 
   for (const obj of mapping) {
@@ -196,7 +208,7 @@ const handleGameEvent = async (eventName, value, context) => {
         break;
     }
     if (play) {
-      console.log({obj})
+      logger.debug({obj})
       for (const conn of Object.values(connections)) {
         conn.playSound(obj.sound);
       }
@@ -214,7 +226,7 @@ let suppressReport = false;
 watch(configFile, async (event) => {
   if (event === "change") {
     mapping = await config.json();
-    console.log("reload config file!")
+    logger.info("reload config file!")
   }
 });
 
@@ -240,7 +252,6 @@ app.get('/', async (c) => {
 
 app.post('/', async (c) => {
   const payload = await c.req.json();
-  console.log('...............')
   if (payload.previously) {
     const ctx = {
       playerID: payload.player.steamid,
